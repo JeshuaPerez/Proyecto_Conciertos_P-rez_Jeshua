8a3aa0aefb581bff11a67019df11c615323d03f1
Jeshua Perez <odsmanjeshua@gmail.com>
2026-03-26 10:57:05 -0600

    Estructura de HTML para buzon

---
+++ js/buzon.js
@@ -0,0 +1,13 @@
+<section id = "buzon">
+    <h2>Sugerencias De Buzon</h2>
+    <from id ="form-sugerencia">
+        <label for="sugerencia">Describe cual es tu sugerencia</label> <br></br>
+            <textarea name="sugerencia" id="sugerencia" cols="30" rows="10"></textarea>
+                <br></br>
+        <button type="submit">Enviar Sugerencia</button>
+    </from>
+    <div id="lista-sugerencias">
+        <h2>¡Gracias, Recibimos tu sugerencia!</h2>
+        <ul id="sugerencias-lista"></ul>
+    </div>
+    </section>
